1a08d952e1db0f38abc607998a54eae57d450916
Nick Ball <ballnikolas@yahoo.com>
2021-01-31 17:42:19 -0500

    pulled in material UI login form

--- party-on/src/App.js
+++ party-on/src/App.js
@@ -6,8 +6,19 @@ import {
   Link,
   Redirect,
   useHistory,
-  useLocation
+  useLocation,
 } from "react-router-dom";
+import {
+  Button,
+  TextField,
+  Grid,
+  Paper,
+  AppBar,
+  Typography,
+  Toolbar
+  } from "@material-ui/core";
+import styled from "styled-components";
+import logo from './img/party_on.png';
 
 // This example has 3 pages: a public page, a protected
 // page, and a login screen. In order to see the protected
@@ -23,6 +34,20 @@ import {
 // login page? No! You're already logged in. Try it out,
 // and you'll see you go back to the page you visited
 // just *before* logging in, the public page.
+
+const PublicPageHolder = styled.div`
+  height: 100vh;
+  background: #005AA7;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #FFFDE4, #005AA7);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #FFFDE4, #005AA7); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */  
+`;
+
+const Logo = styled.img`
+display: block;
+margin-left: auto;
+margin-right: auto;
+width: 50%;
+`;
 
 export default function AuthExample() {
   return (
@@ -66,7 +91,7 @@ const fakeAuth = {
   signout(cb) {
     fakeAuth.isAuthenticated = false;
     setTimeout(cb, 100);
-  }
+  },
 };
 
 /** For more details on
@@ -77,11 +102,7 @@ const authContext = createContext();
 
 function ProvideAuth({ children }) {
   const auth = useProvideAuth();
-  return (
-    <authContext.Provider value={auth}>
-      {children}
-    </authContext.Provider>
-  );
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
 }
 
 function useAuth() {
@@ -91,14 +112,14 @@ function useAuth() {
 function useProvideAuth() {
   const [user, setUser] = useState(null);
 
-  const signin = cb => {
+  const signin = (cb) => {
     return fakeAuth.signin(() => {
       setUser("user");
       cb();
     });
   };
 
-  const signout = cb => {
+  const signout = (cb) => {
     return fakeAuth.signout(() => {
       setUser(null);
       cb();
@@ -108,7 +129,7 @@ function useProvideAuth() {
   return {
     user,
     signin,
-    signout
+    signout,
   };
 }
 
@@ -146,7 +167,7 @@ function PrivateRoute({ children, ...rest }) {
           <Redirect
             to={{
               pathname: "/login",
-              state: { from: location }
+              state: { from: location },
             }}
           />
         )
@@ -156,7 +177,97 @@ function PrivateRoute({ children, ...rest }) {
 }
 
 function PublicPage() {
-  return <h3>Public</h3>;
+  return (
+    <PublicPageHolder>
+      <div>PartyOn Public Page</div>
+      <Logo alt="PartyOn" src={logo}/>
+      <div>
+<AppBar position="static" alignitems="center" color="primary">
+<Toolbar>
+<Grid container justify="center" wrap="wrap">
+<Grid item>
+<Typography variant="h6">{"BRAND_NAME"}</Typography>
+</Grid>
+</Grid>
+</Toolbar>
+</AppBar>
+<Grid container spacing={0} justify="center" direction="row">
+<Grid item>
+<Grid
+container
+direction="column"
+justify="center"
+spacing={2}
+className="login-form"
+>
+<Paper
+variant="elevation"
+elevation={2}
+className="login-background"
+>
+<Grid item>
+<Typography component="h1" variant="h5">
+Sign in
+</Typography>
+</Grid>
+<Grid item>
+<form onSubmit={console.log("")}>
+<Grid container direction="column" spacing={2}>
+<Grid item>
+<TextField
+type="email"
+placeholder="Email"
+fullWidth
+name="username"
+variant="outlined"
+value=""
+// value={this.state.username}
+onChange={(event) =>
+this.setState({
+[event.target.name]: event.target.value,
+})
+}
+required
+autoFocus
+/>
+</Grid>
+<Grid item>
+<TextField
+type="password"
+placeholder="Password"
+fullWidth
+name="password"
+variant="outlined"
+// value={this.state.password}
+value=""
+onChange={(event) =>
+this.setState({
+[event.target.name]: event.target.value,
+})
+}
+required
+/>
+</Grid>
+<Grid item>
+<Button
+variant="contained"
+color="primary"
+type="submit"
+className="button-block"
+>
+Submit
+</Button>
+</Grid>
+</Grid>
+</form>
+</Grid>
+</Paper>
+</Grid>
+</Grid>
+</Grid>
+</div>
+    </PublicPageHolder>
+  );
 }
 
 function ProtectedPage() {
